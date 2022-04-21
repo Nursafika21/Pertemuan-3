ae415703b456ed93d8844322b1e0bef352337890
nursafika <nursafika054@gmail.com>
2022-04-21 21:30:13 +0700

    Contoh code program Greeter

---
+++ src/main/kotlin/1_GreeterTest.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val `1Greeter` = `1_Greeter`()
+    `1Greeter`.greet()
+    `1Greeter`.text = "Hi"
+    `1Greeter`.greet("Anton")
+    `1Greeter`.greet("Budi")
+    `1Greeter`.text = "Hello programmer"
+    println(`1Greeter`.with_ret_val("Dono"))
+}
